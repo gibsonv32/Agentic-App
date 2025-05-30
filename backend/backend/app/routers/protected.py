c837f52ac4969ac39aa09768bc20bdf1f1c862ca
gibsonv32 <48067682+gibsonv32@users.noreply.github.com>
2025-05-30 14:33:02 -0400

    Delete backend directory

--- backend/backend/app/routers/protected.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, Depends
-from app.routers.dependencies import get_current_user
-
-router = APIRouter()
-
-@router.get('/protected')
-async def protected_route(current_user = Depends(get_current_user)):
-    return {"message": f"Hello {current_user.username}"}
