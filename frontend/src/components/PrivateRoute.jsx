4b9e739b48c86747a61b6efc3cfca4ced573d796
gibsonv32 <48067682+gibsonv32@users.noreply.github.com>
2025-05-30 14:33:15 -0400

    Delete frontend directory

--- frontend/src/components/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
-import { AuthContext } from '../contexts/AuthContext';
-
-export default function PrivateRoute({ children }) {
-  const { token } = useContext(AuthContext);
-  return token ? children : <Navigate to="/login" replace />;
-}
